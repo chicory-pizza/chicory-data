a36a5a02ce13660122e04cab5b59edb157c1c3f4
Ng Yik Phang <ngyikp@gmail.com>
2022-02-25 16:00:34 +0000

    Add 🍕 drawdog console text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // @flow
 
 import convertCoordinatesToLevelId from './convertCoordinatesToLevelId';
+import {drawdogConsoleText} from './drawdogConsoleText';
 import levelData from './level_data.json';
 import LevelInspector from './LevelInspector';
 import LevelSelector from './LevelSelector';
@@ -10,8 +11,10 @@ import {useState} from 'react';
 import styles from './App.module.css';
 
 export default function App(): React$Node {
-	// for your custom queries on the browser console
 	window.levelData = levelData;
+
+	console.log(drawdogConsoleText);
+	console.log('Use `window.levelData` for your custom queries!');
 
 	const [coordinates, setCoordinates] = useState<[number, number, number]>([
 		0, 0, 0,
