cb5c0fddd9d9d884a14c5f15a3bdf71c4ab91645
Ng Yik Phang <ngyikp@gmail.com>
2022-03-10 01:00:39 +0000

    Set consistent HTTP headers for both dev and prod
    Terrain editor was previously not working on prod due to dev env not having CSP headers, try to make both env the same

---
+++ src/setupProxy.js
@@ -0,0 +1,44 @@
+// Hack to set custom headers for dev server
+// https://github.com/facebook/create-react-app/issues/10210#issuecomment-873286336
+
+module.exports = (app) => {
+	app.use((req, res, next) => {
+		// Keep this in sync with /public/_headers
+		const headers = [
+			['X-Powered-By', 'Picnic'],
+			['X-Content-Type-Options', 'nosniff'],
+			['X-XSS-Protection', '0'],
+			['X-Frame-Options', 'DENY'],
+			// ['Strict-Transport-Security', 'max-age=31536000'], // irrelevant during dev
+			['Cross-Origin-Opener-Policy', 'same-origin'],
+			['Cross-Origin-Resource-Policy', 'same-site'],
+			['Permissions-Policy', 'interest-cohort=()'],
+			[
+				'Content-Security-Policy',
+				[
+					"default-src 'self'",
+					"script-src 'self'",
+					"connect-src 'self'",
+					// unsafe-inline: For react-select (https://github.com/JedWatson/react-select/issues/2030)
+					"style-src 'self' 'unsafe-inline'",
+					// data: Inline images
+					// blob: Read images to canvas for terrain editor
+					"img-src 'self' data: blob:",
+					"frame-ancestors 'none'",
+					"base-uri 'none'",
+					"object-src 'none'",
+				].join(';'),
+			],
+			[
+				'Feature-Policy',
+				"accelerometer 'none';ambient-light-sensor 'none';battery 'none';camera 'none';display-capture 'none';geolocation 'none';gyroscope 'none';magnetometer 'none';microphone 'none';midi 'none';payment 'none';usb 'none';xr-spatial-tracking 'none';",
+			],
+		];
+
+		headers.forEach((header) => {
+			res.setHeader(header[0], header[1]);
+		});
+
+		next();
+	});
+};
