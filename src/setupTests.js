1283a37be769f8ab47dd12e883f9ae72d7f13349
Ng Yik Phang <ngyikp@gmail.com>
2022-05-27 00:25:38 +0100

    Fix failing tests
    I tried `jest.fn().mockImplementation()` but it doesn't work ¯\_(ツ)_/¯

--- src/setupTests.js
+++ src/setupTests.js
@@ -8,6 +8,20 @@ jest.mock('./levelEditor/level_data.json');
 
 Element.prototype.scrollIntoView = jest.fn();
 
+// https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+window.matchMedia = (query) => {
+	return {
+		matches: false,
+		media: query,
+		onchange: null,
+		addListener: jest.fn(), // deprecated
+		removeListener: jest.fn(), // deprecated
+		addEventListener: jest.fn(),
+		removeEventListener: jest.fn(),
+		dispatchEvent: jest.fn(),
+	};
+};
+
 // For react-modal
 const fakeAppRoot = document.createElement('div');
 ReactModal.setAppElement(fakeAppRoot);
