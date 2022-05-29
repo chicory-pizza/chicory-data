97fa1b9cb167b7b3de3e8226d5a5955ad0383fd0
Ng Yik Phang <ngyikp@gmail.com>
2022-05-29 12:00:00 +0100

    Add 'go to home' button on header

--- src/header/AppHeader.js
+++ src/header/AppHeader.js
@@ -2,6 +2,8 @@
 
 /* eslint-disable react/jsx-no-target-blank */
 
+import {Link} from 'react-router-dom';
+
 import icon144 from '../icon144.png';
 
 import styles from './AppHeader.module.css';
@@ -17,12 +19,29 @@ type Props = $ReadOnly<{
 export default function AppHeader(props: Props): React$Node {
 	return (
 		<header className={styles.root}>
-			<img
-				src={icon144}
-				alt="Chicory: A Colorful Tale game logo"
-				width={72}
-				height={72}
-			/>
+			<Link className={styles.backToHome} to="/" title="Go to main tools list">
+				{/* https://icons.getbootstrap.com/icons/chevron-left/ */}
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="16"
+					height="16"
+					fill="currentColor"
+					className={styles.backToHomeArrow}
+					viewBox="0 0 16 16"
+				>
+					<path
+						fillRule="evenodd"
+						d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+					/>
+				</svg>
+
+				<img
+					src={icon144}
+					alt="Chicory: A Colorful Tale game logo"
+					width={72}
+					height={72}
+				/>
+			</Link>
 
 			<div className={styles.content}>
 				<div className={styles.leftRight}>
