4af3acd60fb586e4a823ee7863b4a0e2f2a3a8e1
Ng Yik Phang <ngyikp@gmail.com>
2023-04-10 13:37:45 +0100

    Fix CI
    This test failed 3 times 🤷

--- src/levelEditor/preview/__tests__/LevelPreview.js
+++ src/levelEditor/preview/__tests__/LevelPreview.js
@@ -5,7 +5,7 @@ import userEvent from '@testing-library/user-event';
 
 import renderLevelEditorRoute from '../../testUtil/renderLevelEditorRoute';
 
-jest.setTimeout(10000); // hack, sigh
+jest.setTimeout(15000); // hack, sigh
 
 test('checks transforms of decos and objCustomDog', async () => {
 	jest.mock('../../level_data.json', () => ({
