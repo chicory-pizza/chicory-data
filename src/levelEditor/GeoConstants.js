b064a87e16bc792e7c840edef4c9afedc3b55f84
Ng Yik Phang <ngyikp@gmail.com>
2023-04-11 22:56:20 +0100

    DRY/clean up GeoConstants

--- src/levelEditor/GeoConstants.js
+++ src/levelEditor/GeoConstants.js
@@ -4,106 +4,139 @@ export const GEO_WIDTH = 81;
 export const GEO_HEIGHT = 46;
 
 export const SCREEN_WIDTH = 1920;
-export const SCREEN_HEIGHT: number = 1080 + 10; // I don't know why the geo has 10px extra at the bottom...
+export const SCREEN_HEIGHT = 1080 + 10; // I don't know why the geo has 10px extra at the bottom...
+
+const PIXEL_COLOR_WALKABLE = 0;
+const PIXEL_COLOR_RAMP = 1;
+const PIXEL_COLOR_UNSWIMABLE_WALL = 2;
+const PIXEL_COLOR_SWIMABLE_WALL = 3;
+const PIXEL_COLOR_SKY = 4;
+const PIXEL_COLOR_UNWALKABLE_COLLISION = 5;
+const PIXEL_COLOR_WATER = 6;
+const PIXEL_COLOR_HEIGHTS = [
+	255,
+	254,
+	253,
+	252,
+	251,
+	250,
+	249,
+	248,
+	247, // [0, 3, 2]
+	246, // [0, -2, -14]
+	244, // [0, 5, 1]
+	243, // [0, 6, 1]
+	242, // [0, 6, 1]
+];
 
 // Colors
-const EDITOR_UI_PIXEL_COLORS: Map<number, string> = new Map();
-const EXPORT_TERRAIN_PIXEL_COLORS: Map<number, string> = new Map();
+export const EDITOR_UI_PIXEL_COLORS: Map<number, string> = new Map([
+	[PIXEL_COLOR_WALKABLE, '#fff'],
+	[PIXEL_COLOR_RAMP, '#e2e2e2'],
+	[PIXEL_COLOR_UNSWIMABLE_WALL, '#555'],
+	[PIXEL_COLOR_SWIMABLE_WALL, 'pink'],
+	[PIXEL_COLOR_SKY, 'lightblue'],
+	[PIXEL_COLOR_UNWALKABLE_COLLISION, 'brown'],
+	[PIXEL_COLOR_WATER, 'blue'],
+	[PIXEL_COLOR_HEIGHTS[0], '#eeeeee'],
+	[PIXEL_COLOR_HEIGHTS[1], '#e5e5e5'],
+	[PIXEL_COLOR_HEIGHTS[2], '#dddddd'],
+	[PIXEL_COLOR_HEIGHTS[3], '#d4d4d4'],
+	[PIXEL_COLOR_HEIGHTS[4], '#cccccc'],
+	[PIXEL_COLOR_HEIGHTS[5], '#c3c3c3'],
+	[PIXEL_COLOR_HEIGHTS[6], '#bbbbbb'],
+	[PIXEL_COLOR_HEIGHTS[7], '#b2b2b2'],
+	[PIXEL_COLOR_HEIGHTS[8], '#aaaaaa'],
+	[PIXEL_COLOR_HEIGHTS[9], '#a1a1a1'],
+	[PIXEL_COLOR_HEIGHTS[10], '#999999'],
+	[PIXEL_COLOR_HEIGHTS[11], '#909090'],
+	[PIXEL_COLOR_HEIGHTS[12], '#888888'],
+]);
 
-// higher ground layers
-EDITOR_UI_PIXEL_COLORS.set(255, '#eeeeee');
-EDITOR_UI_PIXEL_COLORS.set(254, '#e5e5e5');
-EDITOR_UI_PIXEL_COLORS.set(253, '#dddddd');
-EDITOR_UI_PIXEL_COLORS.set(252, '#d4d4d4');
-EDITOR_UI_PIXEL_COLORS.set(251, '#cccccc');
-EDITOR_UI_PIXEL_COLORS.set(250, '#c3c3c3');
-EDITOR_UI_PIXEL_COLORS.set(249, '#bbbbbb');
-EDITOR_UI_PIXEL_COLORS.set(248, '#b2b2b2');
-EDITOR_UI_PIXEL_COLORS.set(247, '#aaaaaa'); // [0, 3, 2]
-EDITOR_UI_PIXEL_COLORS.set(246, '#a1a1a1'); // [0, -2, -14]
-EDITOR_UI_PIXEL_COLORS.set(244, '#999999'); // [0, 5, 1]
-EDITOR_UI_PIXEL_COLORS.set(243, '#909090'); // [0, 6, 1]
-EDITOR_UI_PIXEL_COLORS.set(242, '#888888'); // [0, 6, 1]
-
-EDITOR_UI_PIXEL_COLORS.set(6, 'blue'); // water
-EDITOR_UI_PIXEL_COLORS.set(5, 'brown'); // unwalkable collision
-EDITOR_UI_PIXEL_COLORS.set(4, 'lightblue'); // sky
-EDITOR_UI_PIXEL_COLORS.set(3, 'pink'); // swimable wall
-EDITOR_UI_PIXEL_COLORS.set(2, '#555'); // unswimable wall
-EDITOR_UI_PIXEL_COLORS.set(1, '#e2e2e2'); // ramp
-EDITOR_UI_PIXEL_COLORS.set(0, '#fff'); // walkable
-
-// higher ground layers
-EXPORT_TERRAIN_PIXEL_COLORS.set(255, '#eeeeee');
-EXPORT_TERRAIN_PIXEL_COLORS.set(254, '#e5e5e5');
-EXPORT_TERRAIN_PIXEL_COLORS.set(253, '#dddddd');
-EXPORT_TERRAIN_PIXEL_COLORS.set(252, '#d4d4d4');
-EXPORT_TERRAIN_PIXEL_COLORS.set(251, '#cccccc');
-EXPORT_TERRAIN_PIXEL_COLORS.set(250, '#c3c3c3');
-EXPORT_TERRAIN_PIXEL_COLORS.set(249, '#bbbbbb');
-EXPORT_TERRAIN_PIXEL_COLORS.set(248, '#b2b2b2');
-EXPORT_TERRAIN_PIXEL_COLORS.set(247, '#aaaaaa'); // [0, 3, 2]
-EXPORT_TERRAIN_PIXEL_COLORS.set(246, '#a1a1a1'); // [0, -2, -14]
-EXPORT_TERRAIN_PIXEL_COLORS.set(244, '#999999'); // [0, 5, 1]
-EXPORT_TERRAIN_PIXEL_COLORS.set(243, '#909090'); // [0, 6, 1]
-EXPORT_TERRAIN_PIXEL_COLORS.set(242, '#888888'); // [0, 6, 1]
-
-EXPORT_TERRAIN_PIXEL_COLORS.set(6, 'blue'); // water
-EXPORT_TERRAIN_PIXEL_COLORS.set(5, 'brown'); // unwalkable collision
-EXPORT_TERRAIN_PIXEL_COLORS.set(4, 'lightblue'); // sky
-EXPORT_TERRAIN_PIXEL_COLORS.set(3, 'pink'); // swimable wall
-EXPORT_TERRAIN_PIXEL_COLORS.set(2, '#555'); // unswimable wall
-EXPORT_TERRAIN_PIXEL_COLORS.set(1, '#e2e2e2'); // ramp
-EXPORT_TERRAIN_PIXEL_COLORS.set(0, '#fff'); // walkable
-
-export {EDITOR_UI_PIXEL_COLORS, EXPORT_TERRAIN_PIXEL_COLORS};
+export const EXPORT_TERRAIN_PIXEL_COLORS: Map<number, string> = new Map([
+	[PIXEL_COLOR_WALKABLE, '#fff'],
+	[PIXEL_COLOR_RAMP, '#e2e2e2'],
+	[PIXEL_COLOR_UNSWIMABLE_WALL, '#555'],
+	[PIXEL_COLOR_SWIMABLE_WALL, 'pink'],
+	[PIXEL_COLOR_SKY, 'lightblue'],
+	[PIXEL_COLOR_UNWALKABLE_COLLISION, 'brown'],
+	[PIXEL_COLOR_WATER, 'blue'],
+	[PIXEL_COLOR_HEIGHTS[0], '#eeeeee'],
+	[PIXEL_COLOR_HEIGHTS[1], '#e5e5e5'],
+	[PIXEL_COLOR_HEIGHTS[2], '#dddddd'],
+	[PIXEL_COLOR_HEIGHTS[3], '#d4d4d4'],
+	[PIXEL_COLOR_HEIGHTS[4], '#cccccc'],
+	[PIXEL_COLOR_HEIGHTS[5], '#c3c3c3'],
+	[PIXEL_COLOR_HEIGHTS[6], '#bbbbbb'],
+	[PIXEL_COLOR_HEIGHTS[7], '#b2b2b2'],
+	[PIXEL_COLOR_HEIGHTS[8], '#aaaaaa'],
+	[PIXEL_COLOR_HEIGHTS[9], '#a1a1a1'],
+	[PIXEL_COLOR_HEIGHTS[10], '#999999'],
+	[PIXEL_COLOR_HEIGHTS[11], '#909090'],
+	[PIXEL_COLOR_HEIGHTS[12], '#888888'],
+]);
 
 // Explanations
 export const PIXEL_COLORS_EXPLANATIONS = [
 	{
 		description: 'Walkable',
-		colors: [0],
+		colors: [PIXEL_COLOR_WALKABLE],
 	},
 	{
 		description: 'Ramp',
-		colors: [1],
+		colors: [PIXEL_COLOR_RAMP],
 	},
 	{
 		description: 'Unswimable wall',
-		colors: [2],
+		colors: [PIXEL_COLOR_UNSWIMABLE_WALL],
 	},
 	{
 		description: 'Swimable wall',
-		colors: [3],
+		colors: [PIXEL_COLOR_SWIMABLE_WALL],
 	},
 	{
 		description: 'Unwalkable collision',
-		colors: [5],
+		colors: [PIXEL_COLOR_UNWALKABLE_COLLISION],
 	},
 	{
 		description: 'Sky',
-		colors: [4],
+		colors: [PIXEL_COLOR_SKY],
 	},
 	{
 		description: 'Water',
-		colors: [6],
+		colors: [PIXEL_COLOR_WATER],
 	},
 	{
 		description: 'Higher ground layers',
-		colors: [255, 254, 253, 252, 251, 250, 249, 248, 247, 246, 244, 243, 242],
+		colors: [
+			PIXEL_COLOR_HEIGHTS[0],
+			PIXEL_COLOR_HEIGHTS[1],
+			PIXEL_COLOR_HEIGHTS[2],
+			PIXEL_COLOR_HEIGHTS[3],
+			PIXEL_COLOR_HEIGHTS[4],
+			PIXEL_COLOR_HEIGHTS[5],
+			PIXEL_COLOR_HEIGHTS[6],
+			PIXEL_COLOR_HEIGHTS[7],
+			PIXEL_COLOR_HEIGHTS[8],
+			PIXEL_COLOR_HEIGHTS[9],
+			PIXEL_COLOR_HEIGHTS[10],
+			PIXEL_COLOR_HEIGHTS[11],
+			PIXEL_COLOR_HEIGHTS[12],
+		],
 	},
 ];
 
 export type ToolbarColorItem = {
 	description: string,
 };
+
 // For use of displaying in toolbar
 const TOOLBAR_COLOR_LOOKUP: Map<number, ToolbarColorItem> = new Map();
 for (const colorItem of PIXEL_COLORS_EXPLANATIONS) {
 	colorItem.colors.forEach((color, arrayIndex) => {
 		let description = colorItem.description;
 		if (description === 'Higher ground layers') {
-			description = 'Height ' + arrayIndex;
+			description = 'Height ' + (arrayIndex + 1);
 		}
 
 		TOOLBAR_COLOR_LOOKUP.set(color, {
