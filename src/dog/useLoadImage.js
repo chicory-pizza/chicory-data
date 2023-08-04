cf1598bdcdbf08eb88c8334b6d86805da7f8dadf
Ng Yik Phang <ngyikp@gmail.com>
2023-08-04 01:55:57 +0100

    Fix issue of dog preview image may not update
    See code comment

--- src/dog/useLoadImage.js
+++ src/dog/useLoadImage.js
@@ -34,6 +34,13 @@ export default function useLoadImage(currentSrc: ?string): ?HTMLImageElement {
 		img.current == null ||
 		img.current.src !== currentSrc // src of previous image may not be fully loaded yet and useEffect hasn't got the chance to run yet, returning it will result in 0x0 image
 	) {
+		if (currentSrc == null && loadedSrc != null) {
+			// Fix issue where removing head skin image, then undo, wouldn't restore the image
+			// My understanding is when the image is removed, loadedSrc is retained
+			// And when you undo, the loadedSrc value still remains due to the old value, therefore the dog preview isn't updating
+			setLoadedSrc(null);
+		}
+
 		return null;
 	}
 
