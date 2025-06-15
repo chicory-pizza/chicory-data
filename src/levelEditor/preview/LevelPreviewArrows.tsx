ca8e7e83373a3b05cdf95ebf270831f7b591b313
Ng Yik Phang <ngyikp@gmail.com>
2025-06-15 11:47:26 +0100

    Enhance LevelPreviewArrows
    - Migrate to Mantine buttons and tooltips
    - Show level name on tooltip

--- src/levelEditor/preview/LevelPreviewArrows.tsx
+++ src/levelEditor/preview/LevelPreviewArrows.tsx
@@ -1,8 +1,20 @@
+import {Button, Tooltip} from '@mantine/core';
+import {
+	IconArrowDown,
+	IconArrowLeft,
+	IconArrowRight,
+	IconArrowUp,
+} from '@tabler/icons-react';
+
 import {useCurrentCoordinatesNonNullable} from '../CurrentCoordinatesContext';
+import convertCoordinatesToLevelId from '../util/convertCoordinatesToLevelId';
+import getLevelLabel from '../util/getLevelLabel';
+import {useWorldDataNonNullable} from '../WorldDataContext';
 
 import styles from './LevelPreviewArrows.module.css';
 
 export default function LevelPreviewArrows() {
+	const {worldData} = useWorldDataNonNullable();
 	const [currentCoordinates, setNewCoordinates] =
 		useCurrentCoordinatesNonNullable();
 
@@ -21,7 +33,7 @@ export default function LevelPreviewArrows() {
 		currentCoordinates[1] + 1,
 		currentCoordinates[2],
 	];
-	const goBottom: [number, number, number] = [
+	const goDown: [number, number, number] = [
 		currentCoordinates[0],
 		currentCoordinates[1],
 		currentCoordinates[2] + 1,
@@ -29,41 +41,70 @@ export default function LevelPreviewArrows() {
 
 	return (
 		<>
-			<button
-				className={styles.arrowBox + ' ' + styles.top}
-				onClick={() => setNewCoordinates(goUp)}
-				title={'Go up to level ' + goUp.join(', ')}
-				type="button"
+			<Tooltip
+				label={
+					'Go up to level ' +
+					getLevelLabel(goUp, worldData[convertCoordinatesToLevelId(goUp)])
+				}
 			>
-				↑
-			</button>
+				<Button
+					className={styles.arrowBox + ' ' + styles.top}
+					onClick={() => setNewCoordinates(goUp)}
+					variant="default"
+				>
+					<IconArrowUp size="1em" />
+				</Button>
+			</Tooltip>
 
-			<button
-				className={styles.arrowBox + ' ' + styles.left}
-				onClick={() => setNewCoordinates(goLeft)}
-				title={'Go left to level ' + goLeft.join(', ')}
-				type="button"
+			<Tooltip
+				label={
+					'Go left to level ' +
+					getLevelLabel(goLeft, worldData[convertCoordinatesToLevelId(goLeft)])
+				}
+				position="right"
 			>
-				←
-			</button>
+				<Button
+					className={styles.arrowBox + ' ' + styles.left}
+					onClick={() => setNewCoordinates(goLeft)}
+					variant="default"
+				>
+					<IconArrowLeft size="1em" />
+				</Button>
+			</Tooltip>
 
-			<button
-				className={styles.arrowBox + ' ' + styles.right}
-				onClick={() => setNewCoordinates(goRight)}
-				title={'Go right to level ' + goRight.join(', ')}
-				type="button"
+			<Tooltip
+				label={
+					'Go right to level ' +
+					getLevelLabel(
+						goRight,
+						worldData[convertCoordinatesToLevelId(goRight)]
+					)
+				}
+				position="left"
 			>
-				→
-			</button>
+				<Button
+					className={styles.arrowBox + ' ' + styles.right}
+					onClick={() => setNewCoordinates(goRight)}
+					variant="default"
+				>
+					<IconArrowRight size="1em" />
+				</Button>
+			</Tooltip>
 
-			<button
-				className={styles.arrowBox + ' ' + styles.bottom}
-				onClick={() => setNewCoordinates(goBottom)}
-				title={'Go bottom to level ' + goBottom.join(', ')}
-				type="button"
+			<Tooltip
+				label={
+					'Go down to level ' +
+					getLevelLabel(goDown, worldData[convertCoordinatesToLevelId(goDown)])
+				}
 			>
-				↓
-			</button>
+				<Button
+					className={styles.arrowBox + ' ' + styles.bottom}
+					onClick={() => setNewCoordinates(goDown)}
+					variant="default"
+				>
+					<IconArrowDown size="1em" />
+				</Button>
+			</Tooltip>
 		</>
 	);
 }
